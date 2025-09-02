69061a557042be980ddc290dfecb128e074e0a41
JoseCaicedo-hub <jjaviercaice89@gmail.com>
2025-09-02 11:27:54 -0500

    terminando correcciones

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,7 +5,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
-} from "../controllers/productController.js";
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 // app.js o server.js
 import express from "express";
 import routes from "./routes/productRoutes.js";
+import dotenv from "dotenv";
+import { connect } from "./prismaClient.js";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
-app.use("/api", routes); // todas las rutas están bajo /api
-app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
-dotenv.config();
-app.use(cors()); // Permite peticiones desde otros orígenes (ej: frontend)
 app.use(express.json()); // Permite recibir JSON desde el frontend
+app.use(cors()); // Permite peticiones desde otros orígenes (ej: frontend)
+app.use("/api", routes); // todas las rutas están bajo /api
 
 
 connect();
